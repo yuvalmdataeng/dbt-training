68c4f54661e4fead2ecef20411ab93d28ef71bb8
yuvalm87@gmail.com <yuvalm87@gmail.com>
2022-12-19 08:16:02 +0000

    added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -17,8 +17,10 @@ o.orderid
 , p.category
 , p.productname
 , p.subcategory
+, {{ markup('ordersellingprice','ordercostprice') }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+
